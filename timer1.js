9e9cda7fd6c2dd92bbffe0dc7f7b8b6b211dab9e
joshuavottero <joshuavottero@outlook.com>
2021-11-30 19:07:59 +0000

    linted timer1

--- timer1.js
+++ timer1.js
@@ -2,12 +2,12 @@ const beepTimes = process.argv.slice(2);
 console.log(beepTimes);
 
 for (let time of beepTimes) {
-  let num;
-  if (num = parseInt(time)){
-    if (num >= 0){
+  let num = parseInt(time);
+  if (num) {
+    if (num >= 0) {
       setTimeout(() =>{
         process.stdout.write('\x07');
-      }, parseInt(time) * 1000)
+      }, parseInt(time) * 1000);
     }
    
   }
